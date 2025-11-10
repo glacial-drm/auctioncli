221f3701dd1e6a50647c8d4ce641fee80c482185
Glace <danieladetokunbo05@gmail.com>
2025-11-10 16:51:42 +0000

    regular train-test split working

--- Intent.py
+++ Intent.py
@@ -1,41 +1,52 @@
-
 import os
 from sklearn . model_selection import train_test_split
 from nltk . corpus import stopwords
 from sklearn . feature_extraction . text import CountVectorizer
 from sklearn . feature_extraction . text import TfidfTransformer
 from sklearn . linear_model import LogisticRegression
+from sklearn . metrics import accuracy_score , f1_score , confusion_matrix
+
+# NEXT STEPS -------------------------------------------
+    # GATHER DATA
+    # IMPLEMENT CROSS VALIDATION/BOOTSTRAPPING
 
 # import all data first
 label_dir = {
-" positive ": "../data/positive",
-" negative ": "../data/negative"
+"positive": "../../data/positive",
+"negative": "../../data/negative"
 }
 
 data = []
 labels = []
 
-for label in label_dir . keys () :
-    for file in os . listdir ( label_dir [ label ]) :
-        filepath = label_dir [ label ] + os . sep + file
-        with open ( filepath , encoding ='utf8 ', errors ='ignore ', mode ='r') as review:
-            content = review . read ()
-            data . append ( content )
-            labels . append ( label )
+for label in label_dir.keys () :
+    for file in os.listdir(label_dir [ label]) :
+        filepath = label_dir [ label]+ os.sep + file
+        with open(filepath, encoding ='utf8 ', errors ='ignore ', mode ='r') as review:
+            content = review.read()
+            data.append(content)
+            labels.append(label)
 
-X_train , X_test , y_train , y_test = train_test_split ( data , labels , stratify = labels , test_size =0.25 , random_state =42)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, stratify = labels, test_size =0.25, random_state =42)
 
-count_vect = CountVectorizer ( stop_words = stopwords . words ('english'))
-X_train_counts = count_vect . fit_transform ( X_train )
+count_vect = CountVectorizer(stop_words = stopwords.words ('english'))
+X_train_counts = count_vect.fit_transform(X_train)
 
-tfidf_transformer = TfidfTransformer ( use_idf = True , sublinear_tf = True ). fit (
-X_train_counts )
-X_train_tf = tfidf_transformer . transform ( X_train_counts )
+tfidf_transformer = TfidfTransformer(use_idf = True, sublinear_tf = True).fit (
+X_train_counts)
+X_train_tf = tfidf_transformer.transform(X_train_counts)
 
-classifier = LogisticRegression ( random_state =0) . fit ( X_train_tf , y_train )
+clf = LogisticRegression ( random_state =0) . fit ( X_train_tf , y_train )
 
+X_new_counts = count_vect . transform ( X_test )
+X_new_tfidf = tfidf_transformer . transform ( X_new_counts )
 
+predicted = clf . predict ( X_new_tfidf )
+
+print ( confusion_matrix ( y_test , predicted ))
+print ( accuracy_score ( y_test , predicted ))
+print ( f1_score ( y_test , predicted , pos_label ='positive') )
 # Export Model
 import pickle
 with open("./pickle/classifier.pickle","wb") as log_reg_classifier:
-    pickle.dump(classifier, log_reg_classifier)
+    pickle.dump(clf, log_reg_classifier)
