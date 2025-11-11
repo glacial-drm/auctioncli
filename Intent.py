cfd14bf1406ca256fa8db18d790aa0898bdd0f65
Glace <danieladetokunbo05@gmail.com>
2025-11-11 11:41:02 +0000

    random whitespace

--- Intent.py
+++ Intent.py
@@ -1,10 +1,10 @@
 import os
-from sklearn . model_selection import train_test_split
-from nltk . corpus import stopwords
-from sklearn . feature_extraction . text import CountVectorizer
-from sklearn . feature_extraction . text import TfidfTransformer
-from sklearn . linear_model import LogisticRegression
-from sklearn . metrics import accuracy_score , f1_score , confusion_matrix
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score , f1_score , confusion_matrix
 
 # NEXT STEPS -------------------------------------------
     # GATHER DATA
@@ -54,22 +54,22 @@ tfidf_transformer = TfidfTransformer(use_idf = True, sublinear_tf = True).fit (
 X_train_counts)
 X_train_tf = tfidf_transformer.transform(X_train_counts)
 
-clf = LogisticRegression ( random_state =0) . fit ( X_train_tf , y_train )
+clf = LogisticRegression(random_state =0).fit(X_train_tf , y_train)
 
-X_new_counts = count_vect . transform ( X_test )
-X_new_tfidf = tfidf_transformer . transform ( X_new_counts )
+X_new_counts = count_vect.transform(X_test)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
-predicted = clf . predict ( X_new_tfidf )
+predicted = clf.predict(X_new_tfidf)
 
-print ( confusion_matrix ( y_test , predicted ))
-print ( accuracy_score ( y_test , predicted ))
-# print ( f1_score ( y_test , predicted , pos_label ='positive') )
+print(confusion_matrix(y_test , predicted))
+print(accuracy_score(y_test , predicted))
+# print(f1_score(y_test , predicted , pos_label ='positive'))
 # Export Model
 
 new_data = [""]
-processed_newdata = count_vect . transform ( new_data )
-processed_newdata = tfidf_transformer . transform ( processed_newdata )
-print ( clf . predict ( processed_newdata ))
+processed_newdata = count_vect.transform(new_data)
+processed_newdata = tfidf_transformer.transform(processed_newdata)
+print(clf.predict(processed_newdata))
 
 import pickle
 with open("./pickle/classifier.pickle","wb") as log_reg_classifier:
